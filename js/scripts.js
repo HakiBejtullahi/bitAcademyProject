671aff7a7079b65393e87b1b7279535cc9cfb3c0
HakiBejtullahi <2570025700a@gmail.com>
2022-06-04 22:21:30 +0200

    about-commit
    about-section commit draft

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,40 @@
 // Document Ready
-$(document).ready(function () {});
+$(document).ready(function () {
+  $(".about-carosel").slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ],
+  });
+});
